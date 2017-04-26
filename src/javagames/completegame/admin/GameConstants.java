809241a2a9931e5cbc92533753b52442ed430b90
Andres <ajward@unomaha.edu>
2017-04-26 14:21:34 -0500

    Prevent resizing of game

--- src/javagames/completegame/admin/GameConstants.java
+++ src/javagames/completegame/admin/GameConstants.java
@@ -11,15 +11,14 @@ import java.awt.Color;
  */
 public class GameConstants {
 
-  public static final Color   APP_BORDER     = Color.DARK_GRAY;
-  public static final int     APP_WIDTH      = 1280;
-  public static final int     APP_HEIGHT     = 720;
-  public static final long    APP_SLEEP      = 1L;
-  public static final String  APP_TITLE      = "Put Game name here";
-  public static final float   WORLD_WIDTH    = 16.0f;
-  public static final float   WORLD_HEIGHT   = 9.0f;
-  public static final float   BORDER_SCALE   = 1f;
-  public static final boolean DISABLE_CURSOR = true;
-  public static final boolean MAINTAIN_RATIO = true;
-
+	public static final Color		APP_BORDER			= Color.DARK_GRAY;
+	public static final int			APP_WIDTH				= 1280;
+	public static final int			APP_HEIGHT			= 720;
+	public static final long		APP_SLEEP				= 1L;
+	public static final String	APP_TITLE				= "Put Game name here";
+	public static final float		WORLD_WIDTH			= 16.0f;
+	public static final float		WORLD_HEIGHT		= 9.0f;
+	public static final float		BORDER_SCALE		= 1f;
+	public static final boolean	DISABLE_CURSOR	= true;
+	public static final boolean	MAINTAIN_RATIO	= false;
 }
--- src/javagames/util/WindowFramework.java
+++ src/javagames/util/WindowFramework.java
@@ -14,61 +14,62 @@ import java.awt.event.ComponentEvent;
  */
 public class WindowFramework extends GameFramework {
 
-  private Canvas canvas;
+	private Canvas canvas;
 
-  @Override
-  protected void createFramework() {
-    canvas = new Canvas();
-    canvas.setBackground(appBackground);
-    canvas.setIgnoreRepaint(true);
-    getContentPane().add(canvas);
-    setLocationByPlatform(true);
-    if (appMaintainRatio) {
-      getContentPane().setBackground(appBorder);
+	@Override
+	protected void createFramework() {
+		canvas = new Canvas();
+		canvas.setBackground(appBackground);
+		canvas.setIgnoreRepaint(true);
+		getContentPane().add(canvas);
+		setLocationByPlatform(true);
+		if (appMaintainRatio) {
+			getContentPane().setBackground(appBorder);
+			// Andres added the 40 pixels to the width and height to
+			// account for the JFrame needing to be bigger than the canvas
+			setSize(appWidth + 40, appHeight + 40);
+			canvas.setSize(appWidth, appHeight); // bugfix Jan 2015
+			setLayout(null);
+			getContentPane().addComponentListener(new ComponentAdapter() {
 
-      // Andres added the 40 pixels to the width and height to
-      // account for the JFrame needing to be bigger than the canvas
-      setSize(appWidth + 40, appHeight + 40);
-      canvas.setSize(appWidth, appHeight); // bugfix Jan 2015
-      setLayout(null);
-      getContentPane().addComponentListener(new ComponentAdapter() {
-        @Override
-        public void componentResized(final ComponentEvent e) {
-          onComponentResized(e);
-        }
-      });
-    }
-    else {
-      canvas.setSize(appWidth, appHeight);
-      pack();
-    }
-    setTitle(appTitle);
-    setupInput(canvas);
-    setVisible(true);
-    createBufferStrategy(canvas);
-    canvas.requestFocus();
-  }
+				@Override
+				public void componentResized(final ComponentEvent e) {
+					onComponentResized(e);
+				}
+			});
+		}
+		else {
+			canvas.setSize(appWidth, appHeight);
+			setResizable(false);
+			pack();
+		}
+		setTitle(appTitle);
+		setupInput(canvas);
+		setVisible(true);
+		createBufferStrategy(canvas);
+		canvas.requestFocus();
+	}
 
-  protected void onComponentResized(final ComponentEvent e) {
-    final Dimension size = getContentPane().getSize();
-    setupViewport(size.width, size.height);
-    canvas.setLocation(vx, vy);
-    canvas.setSize(vw, vh);
-  }
+	protected void onComponentResized(final ComponentEvent e) {
+		final Dimension size = getContentPane().getSize();
+		setupViewport(size.width, size.height);
+		canvas.setLocation(vx, vy);
+		canvas.setSize(vw, vh);
+	}
 
-  @Override
-  public int getScreenWidth() {
-    return canvas.getWidth();
-  }
+	@Override
+	public int getScreenWidth() {
+		return canvas.getWidth();
+	}
 
-  @Override
-  public int getScreenHeight() {
-    return canvas.getHeight();
-  }
+	@Override
+	public int getScreenHeight() {
+		return canvas.getHeight();
+	}
 
-  @Override
-  protected void renderFrame(final Graphics g) {
-    g.clearRect(0, 0, getScreenWidth(), getScreenHeight());
-    render(g);
-  }
+	@Override
+	protected void renderFrame(final Graphics g) {
+		g.clearRect(0, 0, getScreenWidth(), getScreenHeight());
+		render(g);
+	}
 }
