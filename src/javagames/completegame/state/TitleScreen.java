959027a7299ac8bf2bbd0cbc2bd5038baf61e81a
Andres <ajward@unomaha.edu>
2017-04-30 20:51:04 -0500

    Update game title

--- src/javagames/completegame/state/TitleScreen.java
+++ src/javagames/completegame/state/TitleScreen.java
@@ -21,18 +21,16 @@ import javagames.util.Utility;
 public class TitleScreen extends State {
 
   protected KeyboardInput keys;
-  protected Dialogue dialog = new Dialogue();
-  int index = 0;
-  private String[] text = new String[23];
-  private String[] msg = { "GAME TITLE", "", "", "", "PRESS SPACE TO PLAY", "",
-  "PRESS ESC TO EXIT" };
-  
-  public TitleScreen()
-  {
-	  for(int x = 0; x < text.length; x++)
-	  {
-		  text[x] = "";
-	  }
+  protected Dialogue      dialog = new Dialogue();
+  int                     index  = 0;
+  private String[]        text   = new String[23];
+  private String[]        msg    = { "WONDER KEITH AND NO BROTHERS", "", "", "",
+      "PRESS SPACE TO PLAY", "", "PRESS ESC TO EXIT" };
+
+  public TitleScreen() {
+    for (int x = 0; x < text.length; x++) {
+      text[x] = "";
+    }
   }
 
   @Override
@@ -48,24 +46,21 @@ public class TitleScreen extends State {
   public void processInput(final float delta) {
     super.processInput(delta);
 
-    if(keys.keyDownOnce(KeyEvent.VK_SPACE))
-    {
-    	if(index < text.length)
-    	{
-	    	msg = null;
-	    	text[index] = dialog.LevelOneDialogue();
-	    	index++;
-    	}
-    	else
-    	{
-    		text = null;
-    	}
+    if (keys.keyDownOnce(KeyEvent.VK_SPACE)) {
+      if (index < text.length) {
+        msg = null;
+        text[index] = dialog.LevelOneDialogue();
+        index++;
+      }
+      else {
+        text = null;
+      }
     }
-    if (keys.keyDownOnce(KeyEvent.VK_SPACE) && text == null) {
+    if (keys.keyDownOnce(KeyEvent.VK_SPACE) && (text == null)) {
       final GameState state = new GameState();
       state.setLevel(1);
       state.setHearts(3);
-      getController().setState(new LevelStarting(state));
+      getController().setState(new LevelStarting1(state));
     }
   }
 
@@ -82,14 +77,11 @@ public class TitleScreen extends State {
     g.setFont(new Font("Arial", Font.PLAIN, 20));
     g.setColor(Color.ORANGE);
 
-
-    if(msg != null)
-    {
-        Utility.drawCenteredString(g, width, height / 3, msg);    	
+    if (msg != null) {
+      Utility.drawCenteredString(g, width, height / 3, msg);
     }
-    if(text[0] != null)
-    {
-    	Utility.drawString(g, 10, 10, text);
-    }	
+    if (text[0] != null) {
+      Utility.drawString(g, 10, 10, text);
+    }
   }
 }
